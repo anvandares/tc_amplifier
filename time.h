0d693c4ae3136b2ccd115bf091e12a677b24d9b1
livDan <liv.danielsdottir@chasacademy.se>
2024-08-07 10:01:41 +0200

    Olika funktioner för tidshantering

---
+++ time.h
@@ -0,0 +1,69 @@
+
+/*--------------------------------------FOR TIME------------------------------*/
+
+#include "RTC.h"// Include the RTC library
+#include <NTPClient.h> //Include the NTP library
+#include <WiFiUdp.h>
+
+
+WiFiUDP Udp; // A UDP instance to let us send and receive packets over UDP
+NTPClient timeClient(Udp);
+unsigned long timerStarted=0UL; //That the variable can be reached in get_timer()
+
+String get_time() //Return the time 
+{
+  char cstr[20];
+  RTCTime currentTime;
+  RTC.getTime(currentTime); 
+  int year =currentTime.getYear();
+  int month=int(currentTime.getMonth())+1;
+  int day=currentTime.getDayOfMonth();
+  int hour=currentTime.getHour();
+  int minute=currentTime.getMinutes();
+  int seconds=currentTime.getSeconds();  
+
+  sprintf_P(cstr, (PGM_P)F("%04d-%02d-%02dT%02d:%02d:%02d"), year, month, day, hour, minute, seconds);
+  
+  return cstr;
+  }
+void set_timer()
+{
+  timerStarted=millis();//Note millis that have passed since program started to when timer was set.
+  Serial.println(timerStarted); //Prints the time set_timer() was called in millis(), just for debugging
+}
+String get_timer()
+{
+  unsigned long timerStopped=millis(); //Note millis that have passed since program started to when timer was stopped
+  unsigned long timerTime = (timerStopped - timerStarted)/1000; //Total time between timerStarted and timerStopped in seconds
+  unsigned int timerSec = timerTime % 60; //Sec
+  unsigned int timerMin = (timerTime/60)%60;  //Min
+  unsigned int timerHour = (timerTime / (60 * 60)); //I won't bother getting days, one can hope that the temperature will stabilize whitin 24-hours. 
+  char gt[20]; 
+
+  sprintf_P(gt,(PGM_P)F("%02d:%02d:%02d"), timerHour, timerMin, timerSec); 
+  
+  //Prints for debugging.
+  Serial.println("timer stopped at:");
+  Serial.println(gt);
+  Serial.println(timerStopped);
+  Serial.println("timerTime");
+  Serial.println(timerTime);
+  Serial.println("timerSec");
+  Serial.println(timerSec);
+
+  return gt;
+  
+  
+}
+static bool _delay(unsigned long timeStamp, unsigned long hourGlass){ /*Checks if time has passed between readings. Takes timeStamp (time when reading was made), 
+                                                                        and hourGlass (time you want to have passed before next reading is made.) as arguments.*/
+    if (millis() - timeStamp > hourGlass) 
+    {       
+            return (true);               
+    }
+    else
+    {   
+        return (false);
+    }
+  
+}
