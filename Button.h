2834c64a322822d5ace224d34c89d515b811f9d0
livDan <liv.danielsdottir@chasacademy.se>
2024-08-07 09:59:38 +0200

    Logik för knapp

--- Button.h
+++ Button.h
@@ -1,50 +1,47 @@
-
-  // this constant won't change:
-const int buttonPin = 2;  // the pin that the pushbutton is attached to
-
-
-// Variables will change:
-int buttonPushCounter = 0;  // counter for the number of button presses
-int buttonState = 0;        // current state of the button
-int lastButtonState = 0;    // previous state of the button
-
-int get_mode();
-void oven_mode();
-
-
-void oven_mode()
-{
+const int buttonPin = 12;  // the number of the pushbutton pin
  
-// read the pushbutton input pin:
+// Variables will change:
+unsigned long buttonPushCounter = 0;  // counter for the number of button presses
+int lastButtonState = 0;    // previous state of the button
+int buttonState=0; // the current reading from the input pin
+bool boolState=false; 
+unsigned long get_mode();
+int oven_mode() //GÖRA TILL VOID ELLER INT
+{
   buttonState = digitalRead(buttonPin);
 
+  // compare the buttonState to its previous state
   if (buttonState != lastButtonState) 
   {
+
     // if the state has changed, increment the counter
     if (buttonState == HIGH) 
-    {
+    { 
       // if the current state is HIGH then the button went from off to on:
       buttonPushCounter++;
+      Serial.println();
       Serial.println("Oven is set to: ");
       Serial.println(buttonPushCounter);
-      if (buttonPushCounter>7) //Oven can be set between 1-8
+
+     if (buttonPushCounter>8) //Oven can be set between 1-8
       {
-        buttonPushCounter=0;
+        buttonPushCounter=0;      
+       
       }
+      boolState=true; //if button is pressed once. 
+
     } 
     // Delay a little bit to avoid bouncing
     delay(50);
     
   }
+
   // save the current state as the last state, for next time through the loop
   lastButtonState = buttonState;
-
-
-
+ // return buttonPushCounter;
 }
-int get_mode()
+
+unsigned long get_mode()
 {
-
   return buttonPushCounter;
-
 }
